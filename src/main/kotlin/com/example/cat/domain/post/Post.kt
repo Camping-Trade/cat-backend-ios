7a46247174b9f115c4690243737311883ea7561d
Aurora <aurora@MacBook-Pro.local>
2022-09-04 20:34:43 +0900

    refactor: change user table to member

--- src/main/kotlin/com/example/cat/domain/post/Post.kt
+++ src/main/kotlin/com/example/cat/domain/post/Post.kt
@@ -4,18 +4,19 @@ import com.example.cat.domain.User
 import javax.persistence.*
 
 @Entity
-@Table(name = "sale_post")
+@Table(name = "share_post")
 class Post(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "member_id")
     val writer: User,
     val title: String,
     val content: String,
     val imageUrl: String? = null,
     val location : String? = null,
     val campId: String,
+    @Enumerated(EnumType.STRING)
     val type: PostType = PostType.SHARE,
 ) : DateTime()
